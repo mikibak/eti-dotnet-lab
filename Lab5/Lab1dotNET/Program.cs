1cb1135c36c8af07a4fdc0d60978f2d5cf0378d1
mikibak <107919074+mikibak@users.noreply.github.com>
2023-03-02 21:46:44 +0100

    Lab 1.4

--- Lab5/Lab1dotNET/Program.cs
+++ Lab5/Lab1dotNET/Program.cs
@@ -30,21 +30,42 @@ public static class MyExtensions
         if((attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly) 
         {
             rahs += "r";
-        }
+        } 
+        else rahs += "_";
+
         if ((attributes & FileAttributes.Archive) == FileAttributes.Archive)
         {
             rahs += "a";
-        }
+        } 
+        else rahs += "_";
+
         if ((attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
         {
             rahs += "h";
-        }
+        } 
+        else rahs += "_";
+
         if ((attributes & FileAttributes.System) == FileAttributes.System)
         {
             rahs += "s";
         }
+        else rahs += "_";
 
         return rahs;
+    }
+
+    public static int CalculateLength(this DirectoryInfo root, int length = 0)
+    {
+        DirectoryInfo[] Dirs = root.GetDirectories();
+        FileInfo[] Files = root.GetFiles("*");
+
+        length += Files.Length;
+
+        foreach (DirectoryInfo dir in Dirs)
+        {
+            length = dir.CalculateLength(length);
+        }
+        return length;
     }
 }
 
@@ -58,30 +79,34 @@ class program
         }
     }
 
-    public static void Task2(string arg, string tabs)
+    public static void Task2(string arg, string tabs = "")
     {
-        tabs += "   ";
+        tabs += "     ";
 
         DirectoryInfo root = new DirectoryInfo(arg);
         DirectoryInfo[] Dirs = root.GetDirectories();
         FileInfo[] Files = root.GetFiles("*");
-        int elements_in_root = 0;
 
         foreach (DirectoryInfo dir in Dirs)
         {
-            string line = tabs + dir.Name;
-            Console.WriteLine(line);
+            //string line = tabs + dir.Name + " (" + dir.CalculateLength() + ") " + dir.GetDosAttributes();
+            Console.Write(tabs + dir.Name);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(" (" + dir.CalculateLength() + ") ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(dir.GetDosAttributes());
             Task2(dir.FullName, tabs);
-            elements_in_root++;
-        }
-        foreach (FileInfo file in Files)
-        {
-            string line = tabs + file.Name + "  " + file.GetDosAttributes();
-            Console.WriteLine(line + " " + file.Length + " B");
-            elements_in_root++;
         }
 
-        //Console.WriteLine(root.FindOldest(DateTime.MaxValue));
+        foreach (FileInfo file in Files)
+        {
+            //string line = tabs + file.Name + "  " + file.GetDosAttributes();
+            Console.Write(tabs + file.Name);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" " + file.Length + " B ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(file.GetDosAttributes());
+        }
     }
 
     public static void Task3(string arg)
@@ -96,7 +121,7 @@ class program
     static void Main(string[] args)
     {
         program.Task1(args);
-        program.Task2(args[0], "   ");
+        program.Task2(args[0]);
         program.Task3(args[0]);
     }
 
