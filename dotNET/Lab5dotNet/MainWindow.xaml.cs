05034f8b2cc2591f3b16d1433476f257b31ab11f
mikibak <107919074+mikibak@users.noreply.github.com>
2023-04-08 20:06:14 +0200

    Lab 5 task 1ABC

--- dotNET/Lab5dotNet/MainWindow.xaml.cs
+++ dotNET/Lab5dotNet/MainWindow.xaml.cs
@@ -15,15 +15,113 @@ using System.Windows.Shapes;
 
 namespace Lab5dotNet
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public int N { get; set; }
+        public int K { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            N = 5;
+            K = 4;
+            //NewtonSymbol newtonSymbol = new NewtonSymbol(5, 4, calculateNewtonSymbolTasksTextBox);
+        }
 
+        private void nChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            TextBox t = (TextBox)sender;
+            int i;
+            if(int.TryParse(t.Text, out i) && i >= 0 && i <= 9999)
+            {
+                N = i;
+            }
+            else
+            {
+                statusBox.Text = "Write correct number!";
+            }
+        }
+
+        private void kChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            TextBox t = (TextBox)sender;
+            int i;
+            if (int.TryParse(t.Text, out i) && i >= 0 && i <= 9999)
+            {
+                K = i;
+            }
+            else
+            {
+                statusBox.Text = "Write correct number!";
+            }
+        }
+
+        public void CalculateNewtonSymbolTasks(object sender, RoutedEventArgs e)
+        {
+            (int, int) tuple = (N, K);
+            Task<int> taskUpper = new Task<int>((object obj) =>
+            {
+                int result = N;
+                for (int i = 1; i < K; i++)
+                {
+                    result *= (N - i);
+                }
+                return result;
+            }, tuple);
+            taskUpper.Start();
+            taskUpper.Wait();
+
+            Task<int> taskLower = new Task<int>((object obj) =>
+            {
+                int result = 1;
+                for (int i = 1; i <= K; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }, K);
+            taskLower.Start();
+            taskLower.Wait();
+            int result = taskUpper.Result / taskLower.Result;
+            calculateNewtonSymbolTasksTextBox.Text = result.ToString();
+        }
+
+        public void CalculateNewtonSymbolDelegates(object sender, RoutedEventArgs e)
+        {
+            Func<int> upperDelegate = CalculateUpper;
+            Func<int> lowerDelegate = CalculateLower;
+
+            var upper = upperDelegate.BeginInvoke(null, null);//await Task.Run(upperDelegate);
+            var lower = lowerDelegate.BeginInvoke(null, null);
+            int result = upperDelegate.EndInvoke(upper) / lowerDelegate.EndInvoke(lower);
+            calculateNewtonSymbolDelegatesTextBox.Text = result.ToString();
+        }
+
+        public async void CalculateNewtonSymbolAsyncAwait(object sender, RoutedEventArgs e)
+        {
+            var upper = await Task.Run(() => CalculateUpper());
+            var lower = await Task.Run(() => CalculateLower());
+            int result = upper / lower;
+            calculateNewtonSymbolAsyncAwaitTextBox.Text = result.ToString();
+        }
+
+        private int CalculateUpper()
+        {
+            int result = N;
+            for (int i = 1; i < K; i++)
+            {
+                result *= (N - i);
+            }
+            return result;
+        }
+
+        private int CalculateLower()
+        {
+            int result = 1;
+            for (int i = 1; i <= K; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
---
+++ dotNET/Lab5dotNet/NewtonSymbol.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+class NewtonSymbol
+{
+    private TextBox calculateNewtonSymbolTasksTextBox;
+    public int N { get; set; }
+    public int K { get; set; }
+
+    public NewtonSymbol(int N, int K, TextBox calculateNewtonSymbolTasksTextBox)
+    {
+        this.N = N;
+        this.K = K;
+        this.calculateNewtonSymbolTasksTextBox = calculateNewtonSymbolTasksTextBox;
+    }
+
+    
+
+    public void CalculateNewtonSymbolTasks(object sender, RoutedEventArgs e)
+    {
+        (int, int) tuple = (N, K);
+        Task<int> taskUpper = new Task<int>((object obj) =>
+        {
+            int result = N;
+            for (int i = 1; i < K; i++)
+            {
+                result *= (N - i);
+            }
+            return result;
+        }, tuple);
+        taskUpper.Start();
+        taskUpper.Wait();
+
+        Task<int> taskLower = new Task<int>((object obj) =>
+        {
+            int result = 1;
+            for (int i = 1; i <= K; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }, K);
+        taskLower.Start();
+        taskLower.Wait();
+        int result = taskUpper.Result / taskLower.Result;
+        calculateNewtonSymbolTasksTextBox.Text = result.ToString();
+    }
+
+    private int CalculateUpper()
+    {
+        int result = N;
+        for (int i = 1; i < K; i++)
+        {
+            result *= (N - i);
+        }
+        return result;
+    }
+
+    private int CalculateLower()
+    {
+        int result = 1;
+        for (int i = 1; i <= K; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+}
