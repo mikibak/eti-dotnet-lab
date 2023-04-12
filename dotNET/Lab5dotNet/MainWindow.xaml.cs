8b1a91479c53cae6d4c9b45f867d172a81615454
mikibak <107919074+mikibak@users.noreply.github.com>
2023-04-12 14:53:17 +0200

    Lab 5 Resolve DNS

--- dotNET/Lab5dotNet/MainWindow.xaml.cs
+++ dotNET/Lab5dotNet/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.Intrinsics.X86;
+using System.Net;
 //using System.Windows.Forms;
 
 namespace Lab5dotNet
@@ -26,6 +28,14 @@ namespace Lab5dotNet
     {
         public int N { get; set; }
         public int K { get; set; }
+
+        string[] hostNames = { "www.microsoft.com", "www.apple.com",
+            "www.google.com", "www.ibm.com", "cisco.netacad.net",
+            "www.oracle.com", "www.nokia.com", "www.hp.com", "www.dell.com",
+            "www.samsung.com", "www.toshiba.com", "www.siemens.com",
+            "www.amazon.com", "www.sony.com", "www.canon.com", 
+            "www.alcatel-lucent.com", "www.acer.com", "www.motorola.com" };
+
         public MainWindow()
         {
             InitializeComponent();
@@ -101,6 +111,31 @@ namespace Lab5dotNet
             var lower = lowerDelegate.BeginInvoke(null, null);
             int result = upperDelegate.EndInvoke(upper) / lowerDelegate.EndInvoke(lower);
             calculateNewtonSymbolDelegatesTextBox.Text = result.ToString();
+        }
+
+        private static List<Tuple<string, string>> GetAdresses(string[] hostNames)
+        {
+            List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+            hostNames.AsParallel()
+                .ForAll(hostName =>
+                {
+                    lock (result)
+                    {
+                        result.Add(Tuple.Create(hostName, Dns.GetHostAddresses(hostName).First().ToString()));
+                    }
+                });
+
+            return result;
+        }
+
+        public void ResolveDNS(object sender, RoutedEventArgs args)
+        {
+            var domainList = GetAdresses(hostNames);
+            resolveDNSTextBox.Text = "";
+            foreach (var domain in domainList)
+            {
+                resolveDNSTextBox.Text += $"{domain.Item1} => {domain.Item2}\n";
+            }
         }
 
         public async void CalculateNewtonSymbolAsyncAwait(object sender, RoutedEventArgs e)
